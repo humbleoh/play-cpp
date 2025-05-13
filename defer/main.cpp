f0f2fcdd5e209d324977d89c6f37e15f3cc719e0
ohyusheng <humbleoh@gmail.com>
2025-05-14 01:52:11 +0800

    defer: 强制内联，并添加宏来模拟常规 defer 语法形式

--- defer/main.cpp
+++ defer/main.cpp
@@ -1,23 +1,32 @@
 #include <concepts>
 #include <iostream>
 
+#define FORCE_INLINE [[gnu::always_inline]] inline
+
 template<typename T>
   requires std::invocable<T> && std::copy_constructible<T>
 struct Defer : T {
-  Defer(T&& callable)
+  FORCE_INLINE Defer(T&& callable)
     : T { callable }
   {
   }
 
-  ~Defer()
+  FORCE_INLINE ~Defer()
   {
     T::operator()();
   }
 };
 
+#define defer DEFER
+#define DEFER IMPL_DEFER_HELPER(__COUNTER__)
+#define IMPL_DEFER_HELPER(n) IMPL_DEFER(n)
+#define IMPL_DEFER(n) Defer defer_##n = [&]() -> void
+
 int main(int argc, char* argv[])
 {
   Defer d = [](){ std::cout << "defer" << std::endl; };
+  DEFER { std::cout << "defer2" << std::endl; };
+  defer { std::cout << "defer3" << std::endl; };
   std::cout << "Processing" << std::endl;
   return 0;
 }
