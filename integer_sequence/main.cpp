9770bdd2194a48c2a4f48738077bed1c4d3fc4e6
ohyusheng <humbleoh@gmail.com>
2025-05-29 18:38:30 +0800

    添加 integer_sequence 的使用示例

--- integer_sequence/main.cpp
+++ integer_sequence/main.cpp
@@ -34,10 +34,32 @@ constexpr auto make_integer_sequence1() noexcept
   return typename make_integer_sequence_impl<N>::type{};
 }
 
+template<typename... Ts>
+using integer_sequence_for = make_integer_sequence0<sizeof...(Ts)>;
+
 template<std::size_t... Ns>
 void print_integer_sequence(integer_sequence<Ns...>)
 {
   ((std::cout << Ns << std::endl), ...);
+}
+
+template<std::size_t I, typename T>
+void print_type_mapping_impl(integer_sequence<I>, T&& arg)
+{
+  std::cout << I << ' ' << arg << std::endl;
+}
+
+template<std::size_t I, std::size_t... Is, typename T, typename... Ts>
+void print_type_mapping_impl(integer_sequence<I, Is...> seq, T&& arg, Ts&&... args)
+{
+  print_type_mapping_impl(integer_sequence<I>{}, std::forward<T>(arg));
+  print_type_mapping_impl(integer_sequence<Is...>{}, std::forward<Ts>(args)...);
+}
+
+template<typename... Ts>
+void print_type_mapping(Ts&&... args)
+{
+  print_type_mapping_impl(integer_sequence_for<Ts...>{}, std::forward<Ts>(args)...);
 }
 
 int main(int argc, char* argv[])
@@ -45,6 +67,8 @@ int main(int argc, char* argv[])
   print_integer_sequence(make_integer_sequence0<5>{});
   std::cout << "==>" << std::endl;
   print_integer_sequence(make_integer_sequence1<6>());
+  std::cout << "==>" << std::endl;
+  print_type_mapping("hello", 1000, "world", 0.1278, 'a');
   return 0;
 }
 
