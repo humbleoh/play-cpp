a2d95e4e00060b2e5880ea32aa51f6d4ce7a72f1
ohyusheng <humbleoh@gmail.com>
2025-05-29 18:03:47 +0800

    添加 integer_sequence 项目

---
+++ integer_sequence/main.cpp
@@ -0,0 +1,47 @@
+#include <concepts>
+#include <iostream>
+#include <utility>
+
+template<std::size_t... Ns>
+struct integer_sequence
+{
+};
+
+// <N-1, N-1, Ns...> 中的首个参数 N-1 是迭代计数，之后的参数列是数列展开。示例：
+// 设 N = 4
+// 迭代 4：3, 3
+// 迭代 3：2, 2, 3
+// 迭代 2：1, 1, 2, 3
+// 迭代 1：0, 0, 1, 2, 3
+// 迭代 0：把 0, 1, 2, 3 传给 integer_sequence 模版参数
+template<std::size_t N, std::size_t... Ns>
+struct make_integer_sequence_impl : make_integer_sequence_impl<N-1, N-1, Ns...> 
+{
+};
+
+template<std::size_t... Ns>
+struct make_integer_sequence_impl<0, Ns...>
+{
+  using type = integer_sequence<Ns...>;
+};
+
+template<std::size_t N>
+constexpr auto make_integer_sequence() noexcept
+{
+  return typename make_integer_sequence_impl<N>::type{};
+}
+
+template<std::size_t... Ns>
+void print_integer_sequence(integer_sequence<Ns...>)
+{
+  ((std::cout << Ns << std::endl), ...);
+}
+
+int main(int argc, char* argv[])
+{
+  print_integer_sequence(make_integer_sequence<5>());
+  std::cout << "==>" << std::endl;
+  print_integer_sequence(make_integer_sequence<6>());
+  return 0;
+}
+
