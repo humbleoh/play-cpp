26c55c26974a6240c487c09cd9087c40e59fce93
ohyusheng <humbleoh@gmail.com>
2025-08-07 23:05:32 +0800

    添加注释

--- defer/main.cpp
+++ defer/main.cpp
@@ -3,6 +3,8 @@
 
 #define FORCE_INLINE [[gnu::always_inline]] inline
 
+// 优于基于 std::unique_ptr 的方式。采用 std::unique_ptr 需要指定非空的指针值，所
+// 以多占用一个指针对象，同时析构函数被调用时会检查成员指针对象是否为空。
 template<typename T>
   requires std::invocable<T> && std::copy_constructible<T>
 struct Defer : T {
